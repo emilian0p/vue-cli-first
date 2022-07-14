58267ce2735aaa158610fba532487cf63e6995fa
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-14 19:13:21 +0200

    title inserito

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleContent msg=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleContent from './components/TitleContent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleContent
   }
 }
 </script>
